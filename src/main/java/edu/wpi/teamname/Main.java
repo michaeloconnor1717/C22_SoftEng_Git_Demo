7df0e4ba28fd37132d8276a809de97e398deeb1e
DilceO <44061575+DilceO@users.noreply.github.com>
2022-01-14 16:40:58 -0500

    Update Main.java
    Added my name :)

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -3,7 +3,7 @@ package edu.wpi.teamname;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Michael O'Connor");
+    System.out.println("Dilce Oliveira ");
 
 
     App.launch(App.class, args);
